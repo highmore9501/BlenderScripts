f9aa84a851edf4b30ceb6779bd7394e615019cd3
highmore9501 <highmore9501@163.com>
2022-07-23 14:36:46 +0800

    Initial commit

---
+++ ArrangBoneLayers.py
@@ -0,0 +1,23 @@
+# 把所有带deform的骨骼移动到第29层，即Rigify默认的DEF层，运行时需要在PoseMode下
+
+import bpy
+
+deformLayerNo = 29
+mchLayerNo = 28
+orgLayerNo = 30
+
+bpy.ops.pose.select_all()
+
+
+def moveToLayer(poseBone, layerNumber):
+    poseBone.bone.layers[layerNumber] = True  # move bone to DEF Layer
+    poseBone.custom_shape = None
+    for i in range(32):
+        if i != layerNumber:
+            poseBone.bone.layers[i] = False  # set other layer to False
+
+
+# If there ARE objects selected then act on all objects
+for poseBone in bpy.context.active_object.pose.bones[:]:
+    if poseBone.bone.use_deform and poseBone.bone.layers[deformLayerNo] is False:
+        moveToLayer(poseBone, deformLayerNo)
---
+++ main.py
@@ -0,0 +1,13 @@
+# 这里存放的都是各种blender脚本
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
