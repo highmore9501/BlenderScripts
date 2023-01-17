543f320ded304fe18846900745ef34db9b9b3d55
highmore9501 <highmore9501@163.com>
2023-01-18 07:43:32 +0800

    no commit

--- __init__.py
+++ __init__.py
@@ -155,26 +155,6 @@ class HippoToolPanel(bpy.types.Panel):
         row = col.row()
         row.operator(ChangerMatOperator.bl_idname,text='批量修改材质属性',icon='NODE_MATERIAL')
 
-        layout.label(text='修改轴心')
-        col = layout.column()
-        row = col.row()
-        row.operator(PivotToLowestOperator.bl_idname,text='批量轴心到低点',icon='EMPTY_AXIS')
-
-        layout.label(text='修改轴心')
-        col = layout.column()
-        row = col.row()
-        row.operator(AddReferencePlaneOperator.bl_idname,text='新增参考平面',icon='SNAP_FACE')
-
-        layout.label(text='锐化物体')
-        col = layout.column()
-        row = col.row()
-        row.operator(ShapenAllOperator.bl_idname,text='锐化多个物体',icon='SNAP_VOLUME')
-
-        layout.label(text='依图片源重命名')
-        col = layout.column()
-        row = col.row()
-        row.operator(RenameByHerfOperator.bl_idname,text='依图片源重命名',icon='IMAGE_PLANE')
-
         layout.label(text='批量复制代理Pivot')
         col = layout.column()
         row = col.row()
@@ -183,6 +163,21 @@ class HippoToolPanel(bpy.types.Panel):
         row.prop(context.scene,"OriginScale")
         row = col.row() 
         row.operator(DistributeObToTargetsOperator.bl_idname,text="批量生成Pivot",icon="PIVOT_CURSOR")
+
+        layout.label(text='其它功能')
+        col = layout.column()
+        row = col.row()
+        row.operator(PivotToLowestOperator.bl_idname,text='批量轴心到低点',icon='EMPTY_AXIS')
+        
+        row = col.row()
+        row.operator(AddReferencePlaneOperator.bl_idname,text='新增参考平面',icon='SNAP_FACE')
+        
+        row = col.row()
+        row.operator(ShapenAllOperator.bl_idname,text='锐化多个物体',icon='SNAP_VOLUME')
+        
+        col = layout.column()
+        row = col.row()
+        row.operator(RenameByHerfOperator.bl_idname,text='依图片源重命名',icon='IMAGE_PLANE')     
 
 
 
