830a975e16644fbcf18e480a00d4aeaeaf35c456
highmore9501 <highmore9501@163.com>
2023-05-24 16:34:10 +0800

    把转换坐标系的功能添加到了blender中

--- operators/GetParticlesMatrices.py
+++ operators/GetParticlesMatrices.py
@@ -1,9 +1,8 @@
 import bpy
 from mathutils import Matrix
-
 import os
 
-from bpy_extras.io_utils import ExportHelper
+from bpy_extras.io_utils import ExportHelper,axis_conversion
 
 # 脚本目标，复制选中粒子系统所有粒子的矩阵，然后保存，以便发送给spoke
 
@@ -24,15 +23,19 @@ class GetParticlesMatricesOperator(bpy.types.Operator,ExportHelper):
         
         # get the folder
         folder_path = os.path.dirname(self.filepath)
-        file_path = os.path.join(folder_path, "{}{}.{}".format(ps.name,"PartilcesInfo", "txt"))   
+        file_path = os.path.join(folder_path, "{}{}.{}".format(ps.name,"PartilcesInfo", "txt"))  
         
+        blender_to_three_matrix = axis_conversion(from_forward='Y', from_up='Z', to_forward='-Z', to_up='Y')
+        # 把blender_to_three_matrix转换为4x4矩阵
+        blender_to_three_matrix = Matrix(blender_to_three_matrix.to_4x4())
         matrices = []
 
-        for p in ps.particles:            
+        for p in ps.particles:    
+                    
             mat_loc = Matrix.Translation(p.location)           
             mat_rot = p.rotation.to_matrix().to_4x4()            
             mat_sca = Matrix.Scale(p.size, 4)            
-            mat = mat_loc @ mat_rot @ mat_sca            
+            mat = blender_to_three_matrix @ mat_loc @ mat_rot @ mat_sca            
             matrices.append(mat)
         
         # 导出矩阵信息到file_path
