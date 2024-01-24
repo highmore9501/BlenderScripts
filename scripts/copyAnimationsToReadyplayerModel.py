52b92966267f3c651b4db448b0bdb115d2c72f83
highmore9501 <highmore9501@163.com>
2024-01-25 04:57:33 +0800

    修改了解析args参数的方法

--- scripts/copyAnimationsToReadyplayerModel.py
+++ scripts/copyAnimationsToReadyplayerModel.py
@@ -3,7 +3,7 @@
 # blender -b e:\readyplayer_model.blend -P E:\BlenderScripts\scripts\copyAnimationsToReadyplayerModel.py -- C:\Users\BigHippo78\Downloads\readyplayer.glb
 
 # 如果改成执行非background模式的指令，也就是下面这个，
-# blender e:\readyplayer_model.blend -P E:\BlenderScripts\scripts\copyAnimationsToReadyplayerModel.py -- C:\Users\BigHippo78\Downloads\readyplayer.glb
+# blender e:\readyplayer_model.blend -P E:\BlenderScripts\scripts\copyAnimationsToReadyplayerModel.py -- C:\Users\BigHippo78\Downloads\readyplayer.glb C:\Users\BigHippo78\Downloads\readyplayer_animated.glb
 # 能正常执行完。
 
 # 以上问题均出现在windows环境，blender版本3.5的情况下，其它情况未测试
@@ -14,20 +14,20 @@ import bpy
 import sys
 import os
 
-# 获取命令行参数（跳过第一个参数，因为它是blender）
-args = sys.argv[1:]
-print("args: ", args)
+# 获取命令行参数中`--`后面的参数
+args = sys.argv[sys.argv.index("--") + 1:]
+
 # 第五个参数是要导入的glb文件路径
-src = args[-1]
+src = args[0]
 src_path = os.path.dirname(src)
 file_name = os.path.basename(src)
 
-target_name = file_name.split(".")[0] + "_animated"
+if len(args) > 1:
+    target = args[1]
+else:
+    target_name = file_name.split(".")[0] + "_animated"
+    target = src_path + "\\\\" + target_name + ".glb"
 
-target = src_path + "\\\\" + target_name + ".glb"
-
-print("src: ", src)
-print("target: ", target)
 # 打开并导入glb文件
 bpy.ops.import_scene.gltf(
     filepath=src,
